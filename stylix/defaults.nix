99c3e5565f4013876566e01c961d2f49e0ec7fbb
nokogiri <nokogiri@gefjon.org>
2024-05-31 21:29:17 +0200

    sway, for now?

--- stylix/defaults.nix
+++ stylix/defaults.nix
@@ -12,9 +12,16 @@
     homeManagerIntegration.followSystem = true;
     image = pkgs.fetchurl {
       #url = "https://gruvbox-wallpapers.pages.dev/wallpapers/light/Kojiro.png";
-      url = "https://raw.githubusercontent.com/Apeiros-46B/everforest-walls/0ae7c31e34385b9af33a74a707b9c4acbfef4d8c/close_up/flowers.png";
-      hash = "sha256-S289wtWBOk8cOqrd1YsRrdBgrDDzREHD7ZRLZ38NJ1I=";
+      #url = "https://raw.githubusercontent.com/Apeiros-46B/everforest-walls/0ae7c31e34385b9af33a74a707b9c4acbfef4d8c/close_up/flowers.png";
+      #url = "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/bulbs.jpg";
+      #url = "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/forest-mountain-cloudy-valley.png";
+      url = "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/tTIlCNT.jpeg";
       #hash = "sha256-JRro+wz9NrVOeLwjWwpQYzy9wG7IyhfGa1cUxr9Wpio=";
+      #hash = "sha256-S289wtWBOk8cOqrd1YsRrdBgrDDzREHD7ZRLZ38NJ1I=";
+      #hash = "sha256-Qm8tpGAfjea3XdvqMoTyPTGPe/mHf5YRLSLbNNGj+C4=";
+      #hash = "sha256-fa1K47o1nBCHApVYj12SGnRym7JpnkU1nvLqA7OyslM=";
+      hash = "sha256-3GZFl8QnxAUJlI5B5taGVFKGp6Asn7UGKXxtDi3lhiQ=";
+
     };
     base16Scheme = ./everforest/everforest-dark-medium.yaml;
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
