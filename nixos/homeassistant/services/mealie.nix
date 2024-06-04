22e818cb63a85ee702d689df18ec650b8ef0ab3a
nokogiri <nokogiri@gefjon.org>
2024-06-04 17:38:21 +0200

    mealie2

--- nixos/homeassistant/services/mealie.nix
+++ nixos/homeassistant/services/mealie.nix
@@ -7,9 +7,10 @@
     listenAddress = "127.0.0.1";
     credentialsFile = config.sops.secrets.mealieCredentials.path;
     settings = {
-      ALLOW_SIGNUP = "true";
+      ALLOW_SIGNUP = "false";
       BASE_URL = "https://mealie.fishoeder.net";
       DB_ENGINE = "sqlite";
+      THEME_DARK_PRIMARY = "#ff0081";
     };
   };
 }
