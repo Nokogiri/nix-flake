7bbe95cc681fe584cb884cf21bb732d4d713927d
nokogiri <nokogiri@gefjon.org>
2024-06-04 15:53:51 +0200

    mealie secrets

--- nixos/homeassistant/services/mealie.nix
+++ nixos/homeassistant/services/mealie.nix
@@ -1,7 +1,11 @@
-{
+{ config, ... }:{
+  sops.secrets.mealieCredentials = {
+    sopsFile = ../../common/secrets.yaml;
+  };
   services.mealie = {
     enable = true;
     listenAddress = "127.0.0.1";
+    credentialsFile = config.sops.secrets.mealieCredentials.path;
     settings = {
       ALLOW_SIGNUP = "true";
       BASE_URL = "https://mealie.fishoeder.net";
