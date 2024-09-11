f9299078f114d60e049971f3c1c1bab55ac522ac
nokogiri <nokogiri@gefjon.org>
2024-09-11 08:59:38 +0200

    vault11

--- nixos/homeassistant/services/nginx/sites/files.fishoeder.net.nix
+++ nixos/homeassistant/services/nginx/sites/files.fishoeder.net.nix
@@ -13,11 +13,11 @@
         "~ ^/org(/.*)?$" = {
           basicAuthFile = config.sops.secrets.davuser.path;
           extraConfig = ''
-            dav_methods = put delete mkcol copy move;
-            dav_access = user:rw;
-            create_full_put_path = on;
+            dav_methods put delete mkcol copy move;
+            dav_access  user:rw;
+            create_full_put_path on;
           '';
-          alias = "/media/Vault31/personal/Org\$1";
+          alias = "/media/Vault3.1/personal/Org\$1";
         };
       };
     };
