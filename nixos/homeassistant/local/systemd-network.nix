90f833decbd8f4f40e6c930bf64d0d189b41dae9
nokogiri <nokogiri@gefjon.org>
2024-09-17 18:13:08 +0200

    nix flake update

--- nixos/homeassistant/local/systemd-network.nix
+++ nixos/homeassistant/local/systemd-network.nix
@@ -1,7 +1,29 @@
-{
+{ config, ... }:{
   imports = [ ../../common/optional/systemd-network.nix ];
   networking.useDHCP = false;
   systemd.network = {
+    netdevs = {
+      "mullvad0" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "mullvad0";
+          MTUBytes = "1300";
+        };
+        # See also man systemd.netdev (also contains info on the permissions of the key files)
+        wireguardConfig = {
+          # Don't use a file from the Nix store as these are world readable. Must be readable by the systemd.network user
+          PrivateKeyFile = "/run/keys/wireguard-privkey";
+          ListenPort = 9918;
+        };
+        wireguardPeers = [
+          {
+            PublicKey = "OhApdFoOYnKesRVpnYRqwk3pdM247j8PPVH5K7aIKX0=";
+            AllowedIPs = ["fc00::1/64" "10.100.0.1"];
+            Endpoint = "{set this to the server ip}:51820";
+          }
+        ];
+      };
+    };
     networks = {
       "wired" = {
         matchConfig.Name = [ "en*" ];
--- nixos/homeassistant/services/default.nix
+++ nixos/homeassistant/services/default.nix
@@ -2,17 +2,16 @@
   imports = [
     ./dave.nix
     ./fail2ban.nix
-    ./flood.nix
+    #./flood.nix
     #./forgejo.nix
     ./nfs-server.nix
-    #./mealie.nix
     ./nix-serve.nix
     ./nginx
     ./samba.nix
     ./podman
     ./postgresql.nix
     ./jellyfin.nix
-    ./transmission.nix
+    #./transmission.nix
     ./vaultwarden.nix
   ];
 }
