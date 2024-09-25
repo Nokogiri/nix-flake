4cd3fb216f18280451af3d5a8ad82dfafac47a07
nokogiri <nokogiri@gefjon.org>
2024-09-25 23:36:26 +0200

    nc

--- nixos/homeassistant/services/postgresql.nix
+++ nixos/homeassistant/services/postgresql.nix
@@ -1,25 +1,15 @@
-{
+{ pkgs, ... }:{
   services.postgresql = {
     enable = true;
     settings = {
       password_encryption = "scram-sha-256";
     };
-    #authentication = "	local    giteadb    gitea    scram-sha-256\n";
     ensureDatabases = [
-      "forgejodb"
-      "forgejo"
-      "paperless"
+      "nextcloud"
     ];
-    ensureUsers = [
-      {
-        name = "forgejo";
-        #ensurePermissions = { "DATABASE forgejodb" = "ALL PRIVILEGES"; };
-        ensureDBOwnership = true;
-      }
-      #{
-      #  name = "paperless";
-      #  ensurePermissions = { "DATABASE paperless" = "ALL PRIVILEGES"; };
-      #}
-    ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method
+      local all       all     trust
+    '';
   };
 }
