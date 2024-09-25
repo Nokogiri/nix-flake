3af931b881336d6bc71cb231aa4f0c39b8f3c419
nokogiri <nokogiri@gefjon.org>
2024-09-25 10:56:37 +0200

    nc

--- nixos/homeassistant/services/nextcloud.nix
+++ nixos/homeassistant/services/nextcloud.nix
@@ -5,6 +5,18 @@
     group = config.users.users.nobody.group;
     mode = "0666";
   };
+  sops.secrets.minio = {
+    sopsFile = ../../common/secrets.yaml;
+    owner = config.users.users.nobody.name;
+    group = config.users.users.nobody.group;
+    mode = "0666";
+  };
+  sops.secrets.minio_nc = {
+    sopsFile = ../../common/secrets.yaml;
+    owner = config.users.users.nobody.name;
+    group = config.users.users.nobody.group;
+    mode = "0666";
+  };
   services.nextcloud = {
     enable = true;
     hostName = "next.fishoeder.net";
@@ -17,5 +29,11 @@
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
     forceSSL = true;
     useACMEHost = "fishoeder.net";
+  };
+  services.minio = {
+    enable = true;
+    listenAddress = "127.0.0.1:9000";
+    consoleAddress = "127.0.0.1:9001";
+    rootCredentialsFile = config.sops.secrets.minio.path;
   };
 }
