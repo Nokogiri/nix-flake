6b18cd75f1de3afe0ac0b7066bf5d67fc1c5cde3
nokogiri <nokogiri@gefjon.org>
2024-09-27 19:46:10 +0200

    stylix

--- home-manager/features/desktop/xdg/user-dirs.nix
+++ home-manager/features/desktop/xdg/user-dirs.nix
@@ -1,16 +1,16 @@
 { config, ... }:{
   xdg.userDirs = {
     enable = true;
-    desktop = "${config.home.homeDirectory}/files/top";
-    documents = "${config.home.homeDirectory}/files/doc";
-    download = "${config.home.homeDirectory}/files/dlc";
-    music = "${config.home.homeDirectory}/files/mus";
-    pictures = "${config.home.homeDirectory}/files/pic";
-    publicShare = "${config.home.homeDirectory}/files/pub";
-    templates = "${config.home.homeDirectory}/files/tpl";
-    videos = "${config.home.homeDirectory}/files/vid";
+    desktop = "${config.home.homeDirectory}/Desktop";
+    documents = "${config.home.homeDirectory}/Files";
+    download = "${config.home.homeDirectory}/Downloads";
+    music = "${config.home.homeDirectory}/Music";
+    pictures = "${config.home.homeDirectory}/Pictures";
+    publicShare = "${config.home.homeDirectory}/Public";
+    templates = "${config.home.homeDirectory}/Templates";
+    videos = "${config.home.homeDirectory}/Videos";
     extraConfig = {
-      XDG_MISC_DIR = "${config.home.homeDirectory}/files/tmp";
+      XDG_MISC_DIR = "${config.home.homeDirectory}/Temp";
     };
   };
 }
