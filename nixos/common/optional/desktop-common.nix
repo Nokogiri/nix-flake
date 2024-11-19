4b750a8e48e3d3ddf41fdd6bcdb4b3548663d123
nokogiri <nokogiri@gefjon.org>
2024-11-19 10:49:18 +0100

    up!

--- nixos/common/optional/desktop-common.nix
+++ nixos/common/optional/desktop-common.nix
@@ -78,8 +78,7 @@
     hyprland = {
       enable = false;
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage =
-        inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
     iio-hyprland = {
       enable = false;
