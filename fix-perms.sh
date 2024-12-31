bc7b601ac7e2bdeae3e3867cff15dca94e6940d5
nokogiri <nokogiri@gefjon.org>
2025-01-01 00:35:22 +0100

    some scripts

---
+++ fix-perms.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /nix/config
+sudo chown nokogiri:users -R .
+sudo chown nokogiri:users -R .git  .github  .gitignore  .sops.yaml  build.sh  fix-perms.sh  flake.lock  flake.nix  home-manager  LICENSE  modules  nixos  nixpkgs.nix  overlays  pkgs  shell.nix  switch.sh
