a194623ae07bebd6c1974adedc77e68563ff1fb0
nokogiri <nokogiri@gefjon.org>
2025-01-03 20:19:12 +0100

    up!

--- nixos/common/optional/plasma.nix
+++ nixos/common/optional/plasma.nix
@@ -30,7 +30,7 @@
     })
     catppuccin-sddm
     inputs.kwin-effects-forceblur.packages.${pkgs.system}.default
-    inputs.kwin-gestures.packages.${pkgs.system}.default
+    #inputs.kwin-gestures.packages.${pkgs.system}.default
     inputs.lightly.packages.${pkgs.system}.darkly-qt5
     inputs.lightly.packages.${pkgs.system}.darkly-qt6
     krohnkite
