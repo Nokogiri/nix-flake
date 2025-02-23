da3dd8e68b7b5397b005c8421e067732cffd4959
nokogiri <nokogiri@gefjon.org>
2025-02-23 16:34:00 +0100

    fixi

--- nixos/homeassistant/services/forgejo.nix
+++ nixos/homeassistant/services/forgejo.nix
@@ -29,10 +29,12 @@ in
       # tokenFile should be in format TOKEN=<secret>, since it's EnvironmentFile for systemd
       tokenFile = config.sops.secrets.forgejo_runner.path;
       labels = [
-        "ubuntu-latest:docker://node:16-bullseye"
-        "ubuntu-22.04:docker://node:16-bullseye"
-        "ubuntu-20.04:docker://node:16-bullseye"
-        "ubuntu-18.04:docker://node:16-buster"     
+        #"ubuntu-latest:docker://node:16-bullseye"
+	"ubuntu-latest:docker://catthehacker/ubuntu:full-latest"
+	"ubuntu-22.04:docker://catthehacker/ubuntu:full-22.04"
+        #"ubuntu-22.04:docker://node:16-bullseye"
+        #"ubuntu-20.04:docker://node:16-bullseye"
+        #"ubuntu-18.04:docker://node:16-buster"     
         ## optionally provide native execution on the host:
         "native:host"
       ];
