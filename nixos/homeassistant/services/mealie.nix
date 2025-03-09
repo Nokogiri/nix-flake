bd924d8749cbde7d832a2b0a0a8e0c52a89b0f81
nokogiri <nokogiri@gefjon.org>
2025-03-09 09:13:43 +0100

    fixi

--- nixos/homeassistant/services/mealie.nix
+++ nixos/homeassistant/services/mealie.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
-  sops.secrets.mealieCredentials = {
+  sops.secrets.mealie_mail = {
     sopsFile = ../../common/secrets.yaml;
     owner = config.users.users.nobody.name; # config.services.mealie.user;
     group = config.users.users.nobody.group;
@@ -8,13 +8,26 @@
   services.mealie = {
     enable = true;
     listenAddress = "127.0.0.1";
-    credentialsFile = config.sops.secrets.mealieCredentials.path;
+    port = 9099;
+    credentialsFile = config.sops.secrets.mealie_mail.path;
     settings = {
       ALLOW_SIGNUP = "false";
       BASE_URL = "https://mealie.fishoeder.net";
       DB_ENGINE = "sqlite";
       THEME_DARK_PRIMARY = "#A92B53";
+      THEME_DARK_ACCENT = "#007A99";
+      THEME_DARK_SECONDARY = "#973542";
+      THEME_DARK_SUCCESS = "#43A047";
+      THEME_DARK_INFO = "#1976D2";
+      THEME_DARK_WARNING = "#FF6D00";
+      THEME_DARK_ERROR = "#EF5350";
       THEME_LIGHT_PRIMARY = "#A92B53";
+      THEME_LIGHT_ACCENT = "#007A99";
+      THEME_LIGHT_SECONDARY = "#973542";
+      THEME_LIGHT_SUCCESS = "#43A047";
+      THEME_LIGHT_INFO = "#1976D2";
+      THEME_LIGHT_WARNING = "#FF6D00";
+      THEME_LIGHT_ERROR = "#EF5350";
       MAX_WORKERS = "3";
       WEB_CONCURRENCY = "1";
       SMTP_USER = "nokogiri@gefjon.org";
--- nixos/homeassistant/services/nginx/sites/mealie.fishoeder.net.nix
+++ nixos/homeassistant/services/nginx/sites/mealie.fishoeder.net.nix
@@ -7,7 +7,7 @@
       forceSSL = true;
       locations."/".extraConfig = ''
         set $upstream_app 10.200.200.1;
-        set $upstream_port 9000;
+        set $upstream_port 9099;
         set $upstream_proto http;
         proxy_pass $upstream_proto://$upstream_app:$upstream_port;
       '';
