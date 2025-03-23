e07f70a06ecefc225f801db270e4a928351fb8b8
nokogiri <nokogiri@gefjon.org>
2025-03-23 17:24:15 +0100

    upd

--- home-manager/features/hyprland/hyprpaper.nix
+++ home-manager/features/hyprland/hyprpaper.nix
@@ -3,10 +3,10 @@
   inputs,
   ...
 }: let
-  img0 = "${../../../images/pink.jxl}";
-  img1 = "${../../../images/tori.jxl}";
-  img2 = "${../../../images/adventure.jxl}";
-  img3 = "${../../../images/scandinavia.jxl}";
+  img0 = "${../../../images/tori.jxl}";
+  #img1 = "${../../../images/pink.jxl}";
+  #img2 = "${../../../images/adventure.jxl}";
+  #img3 = "${../../../images/scandinavia.jxl}";
 in {
   services.hyprpaper = {
     enable = true;
@@ -16,9 +16,9 @@ in {
 
       preload = [
         img0
-        img1
-        img2
-        img3
+        #img1
+        #img2
+        #img3
       ];
 
       wallpaper = [
@@ -26,18 +26,18 @@ in {
       ];
     };
   };
-  home.packages = [
-    (pkgs.writeShellScriptBin "img0" ''
-      ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img0}
-    '')
-    (pkgs.writeShellScriptBin "img1" ''
-      ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img1}
-    '')
-    (pkgs.writeShellScriptBin "img2" ''
-      ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img2}
-    '')
-    (pkgs.writeShellScriptBin "img3" ''
-      ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img3}
-    '')
-  ];
+  #home.packages = [
+  #  (pkgs.writeShellScriptBin "img0" ''
+  #    ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img0}
+  #  '')
+  #  (pkgs.writeShellScriptBin "img1" ''
+  #    ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img1}
+  #  '')
+  #  (pkgs.writeShellScriptBin "img2" ''
+  #    ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img2}
+  #  '')
+  #  (pkgs.writeShellScriptBin "img3" ''
+  #    ${inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland}/bin/hyprctl hyprpaper wallpaper eDP-1,${img3}
+  #  '')
+  #];
 }
