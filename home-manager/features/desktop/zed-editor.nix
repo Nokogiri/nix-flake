944ebf352a44794afe05563d91c954efb2a27345
nokogiri <nokogiri@gefjon.org>
2025-04-02 18:07:01 +0200

    misc upd!

--- home-manager/features/desktop/zed-editor.nix
+++ home-manager/features/desktop/zed-editor.nix
@@ -1,22 +1,22 @@
 {pkgs, ...}: {
-  home.packages = [
-    pkgs.alejandra
-    pkgs.nil
-    pkgs.nixd
+  home.packages = with pkgs; [
+    alejandra
+    nil
+    nixd
+    package-version-server
   ];
 
   programs.zed-editor = {
     enable = true;
-    package = pkgs.zed-editor-fhs;
     extensions = [
       "nix"
       "make"
     ];
     userSettings = {
-      features = {
-        copilot = false;
-        inline_completion_provider = "none";
-      };
+      #features = {
+      #  copilot = false;
+      #  edit_prediction_provider = "none";
+      #};
       collaboration_panel = {
         button = false;
       };
@@ -40,9 +40,18 @@
         };
       };
       assistant = {
-        enabled = false;
+        enabled = true;
+        version = "2";
+        default_open_ai_model = null;
+        default_model = {
+          provider = "zed.dev";
+          model = "claude-3-5-sonnet-latest";
+        };
+
         button = false;
       };
+      hour_format = "hour24";
+      auto_update = false;
       lsp = {
         nixd = {
           settings = {
@@ -52,6 +61,11 @@
           };
         };
         nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+        package-version-server = {
           binary = {
             path_lookup = true;
           };
