d45adc27f57b3f91ce5be88a71423c91556836a5
nokogiri <nokogiri@gefjon.org>
2025-04-03 16:25:05 +0200

    clenaup2

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@ set -e
 
 echo -e "\033[1;34minfo:\033[0m copying files..."
 sudo rm -rf /tmp/nix-config
-sudo cp -r /nix/config /tmp/nix-config
+sudo cp -r ./ /tmp/nix-config
 sudo chown root:root -R /tmp/nix-config
 sudo rm -rf /tmp/nix-config/.git
 
