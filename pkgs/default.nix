933f68a03c953f99c7a57e36f9af3c727f6b6ccb
nokogiri <nokogiri@gefjon.org>
2025-04-06 17:23:33 +0200

    Remove inactive packages from repository

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,22 +2,8 @@
 # You can build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 {pkgs, ...}: {
   amdctl = pkgs.callPackage ./amdctl {};
-  #heroic-unwrapped = pkgs.callPackage ./heroic {
-  #  electron = pkgs.electron_32;
-  #};
-  #heroic = pkgs.callPackage ./heroic/fhsenv.nix {};
-  klassy = pkgs.kdePackages.callPackage ./klassy {};
-  krohnkite = pkgs.callPackage ./krohnkite {};
-  kwin-effects-geometry-change = pkgs.callPackage ./kwin-effects-geometry-change {};
-  #libwg = pkgs.callPackage ./mullvad/libwg.nix {};
-  #mullvad = pkgs.callPackage ./mullvad/mullvad.nix {};
-  #inherit (pkgs.callPackage ./mullvad { }) mullvad;
   nix-stray-roots = pkgs.callPackage ./nix-stray-roots {};
-  panel-system-info = pkgs.callPackage ./panel-system-info {};
-  plasma-window-title-applet = pkgs.callPackage ./plasma-window-title-applet {};
   portmod = pkgs.callPackage ./portmod {};
   rofimoji = pkgs.callPackage ./rofimoji {};
   ryzen-ppd = pkgs.callPackage ./ryzen-ppd {};
-  #vintagestory = pkgs.callPackage ./vintagestory {};
-  vscode-taskexplorer = pkgs.callPackage ./vscode-taskexplorer {};
 }
--- pkgs/vscode-taskexplorer/default.nix
+++
@@ -1,9 +0,0 @@
-{vscode-utils, ...}:
-vscode-utils.buildVscodeMarketplaceExtension {
-  mktplcRef = {
-    name = "vscode-taskexplorer";
-    publisher = "spmeesseman";
-    version = "2.13.2";
-    sha256 = "sha256-7Nk1rxtZMSVPTSRgD2bo0wXB2Vx/YtcZj+LcoDrVCkg=";
-  };
-}
