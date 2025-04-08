416e8d5e3912e4ba6d781086c533ee58f9e0f244
nokogiri <nokogiri@gefjon.org>
2025-04-08 18:03:24 +0200

    Remove commented-out secundofox desktop entry

--- home-manager/features/desktop/xdg/entries.nix
+++ home-manager/features/desktop/xdg/entries.nix
@@ -1,12 +1,5 @@
 {
   xdg.desktopEntries = {
-    #"secundofox" = {
-    #  exec = "firefox -P private %u";
-    #  name = "Secundofox";
-    #  icon = "firefox";
-    #  terminal = false;
-    #  type = "Application";
-    #};
     "secfloorp" = {
       exec = "floorp -P private %u";
       name = "SecFLoorp";
