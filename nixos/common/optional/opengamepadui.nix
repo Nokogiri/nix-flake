723129b5b42f41b5201768658abd756cd19526ae
nokogiri <nokogiri@gefjon.org>
2025-04-12 17:13:58 +0200

    Remove desktop environment configuration files

--- nixos/common/optional/opengamepadui.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  programs.opengamepadui = {
-    enable = true;
-    extraPackages = with pkgs; [gamescope mangohud];
-    gamescopeSession.enable = false;
-    inputplumber.enable = true;
-    powerstation.enable = true;
-  };
-}
