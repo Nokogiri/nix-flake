2491a71422d7d9bb85ddc574840c67e7cdbb60dd
nokogiri <nokogiri@gefjon.org>
2025-04-23 16:17:26 +0200

    Update color scheme to Catppuccin Mocha

--- home-manager/features/desktop/colors.nix
+++ home-manager/features/desktop/colors.nix
@@ -1,9 +1,10 @@
 {
   xdg.configFile."colors.css".text = ''
-    @define-color background-darker rgba(30, 31, 41, 230);
-    @define-color background #282a36;
+    /*@define-color background-gradient 'linear-gradient(180deg,rgba(30, 30, 46, 1) 0%, rgba(30, 30, 46, 0.95) 33%, rgba(49, 50, 68, 0.8) 66%, rgba(49, 50, 68, 0.56) 100%);'*/
+    @define-color background-darker rgba(30, 30, 46, 230);
+    @define-color background #1E1E2E;
     @define-color selection #44475a;
-    @define-color foreground #f8f8f2;
+    @define-color foreground #CDD6F4;
     @define-color comment #6272a4;
     @define-color cyan #8be9fd;
     @define-color green #50fa7b;
